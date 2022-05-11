e9dc28f26c18fc3c6ed05b9164e158dcc7cd9bf2
SIju Yu <sijuyu01@gmail.com>
2022-05-11 12:10:19 +0900

    Calendar frame
    Calendar frame with 7 days a week in 30-31 days

---
+++ src/components/Calendar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="calendar-box">
+      <div class="day-cell" v-for="i in offset" :key="i"></div>
+      <div class="day-cell" v-for="j in daysInMonth" :key="j">test</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calendar",
+  data() {
+    return {
+      offset: 3, //1일이 무슨 요일인지에 따라 비워지는 day-cell을 지정해 준 것(이후 월마다의 정보에 따라 변경가능케 하기)
+      daysInMonth: 31,
+    };
+  },
+};
+</script>
+
+<style>
+.calendar-box {
+  margin-left: 15vw;
+  width: 70vw;
+  border: 2px solid black;
+
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.day-cell {
+  border: 1px solid gray;
+  width: calc(10vw - 2px);
+  height: 160px;
+}
+</style>
